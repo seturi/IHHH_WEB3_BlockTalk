9e3684b3379cf8dd4d80dd6cf70f210b71e581e3
Estrel <kestrel3905@naver.com>
2024-02-29 23:18:09 +0900

    code & timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ethers } from "ethers";
 import { Login, Main } from "./pages/Pages";
@@ -6,73 +6,71 @@ import { abi } from "./abi";
 import "./style.css"
 
 // TODO: 로그인 페이지 내에서 컨트랙트 배포
-const CONTRACT_ADDRESS = "0xB746e393d3687C12A5F2F395f2583aCF02F563AF";
+const CONTRACT_ADDRESS = "0x35bdfc8c7675c450721add735d04645f0eb332f2";
 
 const App = (props) => {
   const [myName, setMyName] = useState(null);
   const [myPublicKey, setMyPublicKey] = useState(null);
+  const [myProvider, setMyProvider] = useState(null);
   const [myContract, setMyContract] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-	const contractABI = abi;
-	let provider;
-	let signer;
+  const contractABI = abi;
+  let provider;
+  let signer;
 
-	async function login() {
-		let res = await connectToMetamask();
+  async function login() {
+    let res = await connectToMetamask();
 
-		if (res === true) {
-		provider = new ethers.providers.Web3Provider(window.ethereum);
-		signer = provider.getSigner();
-		try {
-			const contract = new ethers.Contract(
-				CONTRACT_ADDRESS,
-				contractABI,
-				signer
-			);
-			setMyContract(contract);
-			const address = await signer.getAddress();
-			let present = await contract.checkUserExists(address);
-			let name;
-			if (present) name = await contract.getUsername(address);
-			else {
-				name = prompt("Enter your name", "Guest");
-				if (name === "") name = "Guest";
-				await contract.createAccount(name);
-			}
-			setMyName(name);
-			setMyPublicKey(address);
-			setIsLoggedIn(true);
-		} catch (err) {
-				alert("CONTRACT_ADDRESS not set properly!");
-		}
-		} else {
-			alert("Couldn't connect to MetaMask");
-		}
-		return isLoggedIn;
-	}
+    if (res === true) {
+      provider = new ethers.providers.Web3Provider(window.ethereum);
+      setMyProvider(provider);
+      signer = provider.getSigner();
+      try {
+        const contract = new ethers.Contract(CONTRACT_ADDRESS, contractABI, signer);
+        setMyContract(contract);
+        const address = await signer.getAddress();
+        let present = await contract.checkUserExists(address);
+        let name;
+        if (present) name = await contract.getUsername(address);
+        else {
+          name = prompt("Enter your name", "Guest");
+          if (name === "") name = "Guest";
+          await contract.createAccount(name);
+        }
+        setMyName(name);
+        setMyPublicKey(address);
+        setIsLoggedIn(true);
+      } catch (err) {
+        alert("CONTRACT_ADDRESS not set properly!");
+      }
+    } else {
+      alert("Couldn't connect to MetaMask");
+    }
+    return isLoggedIn;
+  }
 
-	async function connectToMetamask() {
-		try {
-			await window.ethereum.enable();
-			return true;
-		} catch (err) {
-			return false;
-		}
-	}
+  async function connectToMetamask() {
+    try {
+      await window.ethereum.enable();
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
 
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/"
-				element={isLoggedIn ? <Navigate to="/main" /> 
-				: <Login login={async () => login()} />} />
-				<Route path="/main"
-				element={isLoggedIn ? <Main name={myName} address={myPublicKey} myContract={myContract}/>
-				: <Navigate to="/" />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"
+          element={isLoggedIn ? <Navigate to="/main" />
+            : <Login login={async () => login()} />} />
+        <Route path="/main"
+          element={isLoggedIn ? <Main name={myName} address={myPublicKey} myProvider={myProvider} myContract={myContract} />
+            : <Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,74 @@
-import { useState } from "react";
-import { NavBar, SideBar, ChatRoom } from "../components/Components"
+import { useState, useEffect } from "react";
+import { utils } from "ethers";
+import { PulseLoader } from "react-spinners";
 import CopyToClipboard from "react-copy-to-clipboard";
+import { NavBar, SideBar, ChatRoom } from "../components/Components"
 
 export const Main = (props) => {
     const [addModal, setAddModal] = useState(false);
     const [genModal, setGenModal] = useState(false);
     const [entModal, setEntModal] = useState(false);
+    const [code, setCode] = useState(null);
+    const [timeStamp, setTimeStamp] = useState(0);
+    const [validTime, setValidTime] = useState(600);
+    const [minute, setMinute] = useState("10");
+    const [second, setSecond] = useState("00");
+
+    const generateCode = async () => {
+        if (!code) {
+            try {
+                const tx = await props.myContract.generateKeyString();
+                const receipt = await tx.wait();
+                console.log(receipt);
+                const key = receipt.events[0].args.key;
+                const code = utils.toUtf8String(key);
+                setCode(code);
+                const blockNumber = await props.myProvider.getBlockNumber();
+                const block = await props.myProvider.getBlock(blockNumber);
+                const timeStamp = block.timestamp;
+                setTimeStamp(timeStamp);
+            } catch (err) {
+                alert("User rejected transation");
+                setAddModal(true);
+                setGenModal(false);
+            }
+        }
+    }
+
+    const handleGenModal = () => {
+        setAddModal(false);
+        setGenModal(true);
+        generateCode();
+    }
+
+    useEffect(() => {
+        if (timeStamp) {
+            const interval = setInterval(() => {
+                const currentTime = Math.floor(Date.now() / 1000);
+                const elapsedTime = currentTime - timeStamp;
+                setValidTime(600 - elapsedTime);
+                setMinute(Math.floor(validTime / 60).toString().padStart(2, "0"));
+                setSecond((validTime % 60).toString().padStart(2, "0"));
+                console.log(minute, second);
+
+                if (validTime <= 0) {
+                    if (genModal) {
+                        setAddModal(true);
+                        setGenModal(false);
+                        alert("Code has been expired");
+                    }
+
+                    clearInterval(interval);
+                    setCode(null);
+                    setTimeStamp(0);
+                };
+            }, 1000);
+
+            return () => {
+                clearInterval(interval);
+            };
+        }
+    });
 
     const AddModal = () => {
         return (
@@ -13,7 +76,7 @@ export const Main = (props) => {
                 <div className="Body">
                     <span className="Title">Add new chat</span>
                     <div className="Select">
-                        <button className="Generate" onClick={() => { setAddModal(false); setGenModal(true); }}>Generate code</button>
+                        <button className="Generate" onClick={handleGenModal}>Generate code</button>
                         <button className="Enter" onClick={() => { setAddModal(false); setEntModal(true); }}>Enter code</button>
                     </div>
                     <div className="Close">
@@ -24,20 +87,30 @@ export const Main = (props) => {
         )
     };
 
-    const GenModal = (props) => {
+    const GenModal = () => {
         return (
             <div className="GenModal">
                 <div className="Body">
                     <span className="Title">Generate code</span>
-                    <div className="Return">
-                        <CopyToClipboard text={props.code}>
-                            <span className="Code">{props.code}</span>
-                        </CopyToClipboard>
-                    </div>
-                    <div className="Select">
-                        <button onClick={() => { setAddModal(true); setGenModal(false); }}>back</button>
-                        <button onClick={() => setGenModal(false)}>Close</button>
-                    </div>
+                    {(code) ?
+                        <>
+                            <div className="Return">
+                                <CopyToClipboard text={code}>
+                                    <span className="Code">{code}</span>
+                                </CopyToClipboard>
+                                <span className="Timer">{minute}:{second}</span>
+                            </div>
+                            <div className="Select">
+                                <button onClick={() => { setAddModal(true); setGenModal(false); }}>back</button>
+                                <button onClick={() => setGenModal(false)}>Close</button>
+                            </div>
+                        </> : <>
+                            <div className="Loading">
+                                <PulseLoader color="white" />
+                            </div>
+                            <div className="bottom"></div>
+                        </>
+                    }
                 </div>
             </div >
         )
