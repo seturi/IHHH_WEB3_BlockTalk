1c9250b00701266b5973e2e96f5d3129ed3b9489
seturi <48719589+seturi@users.noreply.github.com>
2024-03-04 13:36:32 +0900

    Send Message

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,24 +1,25 @@
-import React, {useState} from "react";
+import React, {useState, useRef, useEffect} from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Message } from "./Components"
 import { ReactComponent as RefImg } from "../images/refresh.svg"
 import { ReactComponent as SendImg } from "../images/send.svg"
 
-function Refresh({ onRefresh }) {
+function Refresh({ handleRefresh }) {
     return (
-        <div className="Refresh" onClick={onRefresh}>
+        <div className="Refresh" onClick={handleRefresh}>
             <RefImg width={28} height={28} fill="white" />
         </div>
     )
 }
 
-function Input({ inputText, handleInputChange }) {
+function Input({ inputText, handleInputChange, handleEnter}) {
     return (
         <div className="Input">
             <input 
                 type="text" 
                 value={inputText} 
                 onChange={handleInputChange} 
+                onKeyDown={handleEnter}
             />
         </div>
     );
@@ -36,21 +37,44 @@ function Send({ handleSend }) {
 export function ChatRoom(props) {
     const [inputText, setInputText] = useState("");
     const [messages, setMessages] = useState([]);
+    const messagePanelRef = useRef(null);
 
     const handleInputChange = (event) => setInputText(event.target.value);
 
     const handleSend = () => {
-        // TODO: 메시지 전송
-        const newMessage = { text: inputText, timestamp: new Date().toISOString() };
+        if (!inputText.trim()) return;
+        
+        const newMessage = { 
+            name: "",
+            text: inputText, 
+            time: new Date().toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' }), 
+            isMine: true, };
         setMessages([...messages, newMessage]);
         setInputText("");
     };
 
-    /* TODO: 새로고침 이벤트 핸들링
-    const handleRefresh = async () => {
+    const handleEnter = (event) => {
+        if (event.key === 'Enter') {
+          handleSend();
+        }
+    };
+
+    const getMessages = async () => {
+        let name;
+        let messages = [];
         
     };
-    */
+
+    const handleRefresh = () => {
+        getMessages();
+    };
+
+    useEffect(() => {
+        getMessages();
+        if (messagePanelRef.current) {
+            messagePanelRef.current.scrollTop = messagePanelRef.current.scrollHeight;
+        }
+    }, [messages.length]);
 
     return (
         <div className="ChatRoom">
@@ -63,17 +87,27 @@ export function ChatRoom(props) {
                         <span className="Address">{props.address.substring(0, 10) + "..."}</span>
                     </CopyToClipboard>
                 </div>
-                <Refresh />
+                <Refresh handleRefresh={handleRefresh}/>
             </div>
-            <div className="MessagePanel">
+            <div className="MessagePanel" ref={messagePanelRef}>
                 <Message name="name1" text="hello" time="00:00" isMine={false} />
-                <Message text="안녕하세요. 만나게 되어 반갑습니다. 어디에서 오셨나요?" time="00:01" isMine={true} />
-                <Message text="대답." time="00:01" isMine={true} />
-                <Message name="name1" text="Wut" time="00:02" isMine={false} />
-                <Message text="대답." time="00:02" isMine={true} />
+                {messages.map((msg, index) =>  {
+                    const nextMsg = messages[index + 1];
+                    const showTime = !nextMsg || nextMsg.time !== msg.time || nextMsg.isMine !== msg.isMine;
+
+                    return (
+                        <Message 
+                            key={index}
+                            name={msg.name} 
+                            text={msg.text} 
+                            time={showTime ? msg.time: undefined} 
+                            isMine={msg.isMine} 
+                        />
+                    );
+                })}
             </div>
             <div className="Bottom">
-                <Input inputText={inputText} handleInputChange={handleInputChange} />
+                <Input inputText={inputText} handleInputChange={handleInputChange} handleEnter={handleEnter} />
                 <Send handleSend={handleSend} />
             </div>
         </div>
